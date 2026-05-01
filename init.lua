7feb0ac41ae2c8449a966165bf11272fc647e723
ronrob-lu <ronrob@emojized.com>
2026-05-01 10:57:28 +0200

    inital setup

---
+++ init.lua
@@ -0,0 +1,293 @@
+-- Menotics Delivery Drone Mod
+menotics_delivery_drone = {}
+menotics_delivery_drone.lamps = {}
+
+local function pos_to_key(pos)
+    if not pos then return nil end
+    return minetest.pos_to_string(vector.round(pos))
+end
+
+local function count_lamps()
+    local c = 0
+    for _ in pairs(menotics_delivery_drone.lamps) do c = c + 1 end
+    return c
+end
+
+local function find_nearest_lamp(current_pos, exclude_pos)
+    local nearest, nearest_dist = nil, math.huge
+    local lamp_positions = {}
+    
+    for lamp_pos_str, _ in pairs(menotics_delivery_drone.lamps) do
+        local lamp_pos = minetest.string_to_pos(lamp_pos_str)
+        if lamp_pos then table.insert(lamp_positions, lamp_pos) end
+    end
+    
+    for _, lamp_pos in ipairs(lamp_positions) do
+        local dist = vector.distance(current_pos, lamp_pos)
+        
+        -- Always skip the lamp we're currently at or very close to
+        if dist > 2 then  -- Must be at least 2 blocks away
+            -- Also skip the previous lamp if we have more than 2 lamps
+            if exclude_pos and count_lamps() > 2 then
+                if vector.equals(lamp_pos, exclude_pos) then goto continue end
+            end
+            
+            if dist < nearest_dist then
+                nearest, nearest_dist = lamp_pos, dist
+            end
+        end
+        ::continue::
+    end
+    return nearest
+end
+
+local function get_sounds()
+    local def = rawget(_G, "default")
+    if def and def.node_sound_wood_defaults then
+        return def.node_sound_wood_defaults()
+    end
+    return nil
+end
+
+-- REGISTER DRONE ENTITY
+minetest.register_entity("menotics_delivery_drone:drone", {
+    initial_properties = {
+        hp_max = 10,
+        physical = false,
+        collisionbox = {-0.3, -0.3, -0.3, 0.3, 0.3, 0.3},
+        visual = "cube",
+        textures = {"menotics.png", "menotics.png", "menotics.png", "menotics.png", "menotics.png", "menotics.png"},
+        visual_size = {x=1, y=1},
+        stepheight = 0,
+        automatic_rotate = 0,
+        gravity = 0,
+    },
+    
+    on_activate = function(self, staticdata, dtime_s)
+        self.state = "idle"
+        self.wait_timer = 0
+        self.target = nil
+        self.last_lamp = nil
+        self.speed = 3
+        self.hover_timer = 0
+        self.inv_name = "drone_" .. tostring(self.object):gsub("[^%w]", "")
+        
+        -- Create persistent inventory
+        self.inv = minetest.create_detached_inventory(self.inv_name, {
+            allow_move = function(inv, from_list, from_index, to_list, to_index, count, player)
+                return count
+            end,
+            allow_put = function(inv, listname, index, stack, player)
+                return stack:get_count()
+            end,
+            allow_take = function(inv, listname, index, stack, player)
+                return stack:get_count()
+            end,
+        })
+        self.inv:set_size("main", 32)
+        
+        -- Load saved data
+        if staticdata and staticdata ~= "" then
+            local data = minetest.deserialize(staticdata)
+            if data then
+                if data.inv_data then
+                    for i=1, 32 do
+                        if data.inv_data[i] then
+                            self.inv:set_stack("main", i, ItemStack(data.inv_data[i]))
+                        end
+                    end
+                end
+                self.state = data.state or "idle"
+                self.wait_timer = data.wait_timer or 0
+            end
+        end
+        
+        minetest.chat_send_all("[Drone] Activated. Lamps: " .. count_lamps())
+    end,
+    
+    on_rightclick = function(self, clicker)
+        if not clicker:is_player() then return end
+        
+        -- Show the inventory formspec
+        minetest.show_formspec(clicker:get_player_name(), "drone:inv",
+            "size[8,9;]"..
+            "list[detached:"..self.inv_name..";main;0,0;8,4;]"..
+            "list[current_player;main;0,4.5;8,4;]"..
+            "listring[]")
+    end,
+    
+    on_step = function(self, dtime)
+        local pos = self.object:get_pos()
+        if not pos then return end
+        
+        self.hover_timer = self.hover_timer + dtime
+        
+        -- Update status text
+        local status_text = "Menotics Delivery Drone\n[" .. self.state:upper() .. "] Lamps: " .. count_lamps()
+        if self.state == "waiting" then
+            status_text = status_text .. "\nWaiting: " .. math.ceil(20 - self.wait_timer) .. "s"
+        elseif self.state == "moving" and self.target then
+            local dist = vector.distance(pos, self.target)
+            status_text = status_text .. "\nDistance: " .. math.floor(dist) .. "m"
+        end
+        self.object:set_properties({infotext = status_text})
+        
+        if self.state == "idle" then
+            self.object:set_velocity({x=0, y=0, z=0})
+            self.target = find_nearest_lamp(pos, self.last_lamp)
+            
+            if self.target then
+                minetest.chat_send_all("[Drone] Flying to " .. minetest.pos_to_string(self.target))
+                self.state = "moving"
+            else
+                minetest.chat_send_all("[Drone] No lamps found to fly to!")
+            end
+            
+        elseif self.state == "moving" then
+            if not self.target then
+                self.state = "idle"
+                return
+            end
+            
+            local dir = vector.subtract(self.target, pos)
+            local dist = vector.length(dir)
+            
+            if dist < 1.5 then
+                self.state = "waiting"
+                self.wait_timer = 0
+                self.last_lamp = pos
+                self.object:set_velocity({x=0, y=0, z=0})
+                minetest.chat_send_all("[Drone] Arrived at checkpoint!")
+                return
+            end
+            
+            local vel = vector.normalize(dir)
+            vel = vector.multiply(vel, self.speed)
+            
+            local hover_y = math.sin(self.hover_timer * 3) * 0.3
+            vel.y = hover_y
+            
+            self.object:set_velocity(vel)
+            
+            local yaw = math.atan2(dir.x, dir.z)
+            self.object:set_yaw(yaw)
+            
+        elseif self.state == "waiting" then
+            self.object:set_velocity({x=0, y=0, z=0})
+            local hover_y = math.sin(self.hover_timer * 3) * 0.3
+            self.object:set_velocity({x=0, y=hover_y, z=0})
+            
+            self.wait_timer = self.wait_timer + dtime
+            
+            if self.wait_timer >= 20 then
+                self.state = "idle"
+                minetest.chat_send_all("[Drone] Resuming delivery route...")
+            end
+        end
+    end,
+    
+    on_punch = function(self, puncher)
+        if puncher and puncher:is_player() then
+            -- Drop all items from inventory
+            local drop_pos = self.object:get_pos()
+            drop_pos.y = drop_pos.y + 1
+            
+            for i=1, 32 do
+                local stack = self.inv:get_stack("main", i)
+                if not stack:is_empty() then
+                    minetest.add_item(drop_pos, stack)
+                end
+            end
+            
+            minetest.chat_send_all("[Drone] Removed - items dropped")
+            minetest.remove_detached_inventory(self.inv_name)
+            self.object:remove()
+        end
+    end,
+    
+    get_staticdata = function(self)
+        -- Save inventory items
+        local inv_data = {}
+        for i=1, 32 do
+            local stack = self.inv:get_stack("main", i)
+            if not stack:is_empty() then
+                inv_data[i] = stack:to_string()
+            end
+        end
+        
+        local data = {
+            inv_data = inv_data,
+            state = self.state,
+            wait_timer = self.wait_timer
+        }
+        
+        -- Clean up inventory
+        minetest.remove_detached_inventory(self.inv_name)
+        
+        return minetest.serialize(data)
+    end,
+    
+    on_deactivate = function(self, removal)
+        if removal then
+            minetest.remove_detached_inventory(self.inv_name)
+        end
+    end,
+})
+
+-- REGISTER LAMP NODE
+minetest.register_node("menotics_delivery_drone:lamp", {
+    description = "Menotics Mese Lamp",
+    tiles = {"default_mese_block.png"},
+    groups = {snappy=2, choppy=2, oddly_breakable_by_hand=2},
+    light_source = 7,
+    sounds = get_sounds(),
+    
+    on_construct = function(pos)
+        menotics_delivery_drone.lamps[pos_to_key(pos)] = true
+        minetest.chat_send_all("[Drone] Lamp placed (Total: " .. count_lamps() .. ")")
+    end,
+    
+    on_destruct = function(pos)
+        menotics_delivery_drone.lamps[pos_to_key(pos)] = nil
+        minetest.chat_send_all("[Drone] Lamp removed (Total: " .. count_lamps() .. ")")
+    end,
+})
+
+-- REGISTER DRONE ITEM
+minetest.register_craftitem("menotics_delivery_drone:drone_item", {
+    description = "Menotics Delivery Drone\nRight-click to place",
+    inventory_image = "menotics.png",
+    
+    on_place = function(itemstack, placer, pointed_thing)
+        if not pointed_thing then return itemstack end
+        
+        local pos = pointed_thing.above
+        if not pos then return itemstack end
+        
+        pos.y = pos.y + 1
+        
+        minetest.add_entity(pos, "menotics_delivery_drone:drone")
+        itemstack:take_item()
+        minetest.chat_send_all("[Drone] Deployed!")
+        return itemstack
+    end,
+})
+
+-- RECIPES
+minetest.register_craft({
+    output = "menotics_delivery_drone:drone_item",
+    recipe = {
+        {"default:steel_ingot", "default:mese_crystal", "default:steel_ingot"},
+        {"default:steel_ingot", "default:chest", "default:steel_ingot"},
+        {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+    }
+})
+
+minetest.register_craft({
+    output = "menotics_delivery_drone:lamp",
+    recipe = {
+        {"default:glass", "default:glass", "default:glass"},
+        {"default:glass", "default:mese_crystal", "default:glass"},
+        {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"},
+    }
+})
---
+++ mod.conf.md
@@ -0,0 +1,5 @@
+name = menotics_delivery_robot
+description = Menotics delivery robots
+author = emojized
+version = 0.0.1
+depends = default
